2a6c6e963003f466588a436f95195613ca7e33be
Josh Weisman <josh.m.weisman@gmail.com>
2016-12-28 13:56:33 +0200

    WDC First Cut

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# ExlTableauWDC
-Tableau Web Data Connector for Ex Libris Analytics
+# Ex Libris Tableau Web Data Connector
+
+This project provides a way to access data from Ex Libris Analytics ([Alma](https://knowledge.exlibrisgroup.com/Alma/Product_Documentation/Alma_Online_Help_(English)/Analytics) or Primo) with [Tableau](http://www.tableau.com/) [Desktop](http://www.tableau.com/products/desktop). To access your Ex Libris data in Tableau, you define a report in Analytics with the desired data. Then add a data source of web data connector to your Tableau workbook for each analytics report you're interested in. Step by step instructions are available at this [blog post](https://developers.exlibrisgroup.com/blog).
+
+For more information on Web Data Connectors, see the Tableau [Online Help](https://onlinehelp.tableau.com/current/pro/desktop/en-us/examples_web_data_connector.html).
+
+## Hosting
+This project is hosted and available at the following URL:
+https://eu-st01.ext.exlibrisgroup.com/delivery/wdc/exl.html
+
+## To Use
+You'll need the following to use the connector:
+1. An API key which has access to the Analytics API. See the Ex Libris [Developer Network}(https://developers.exlibrisgroup.com/alma/apis) for details on how to create and configure an API key.
+2. The path of the desired analytics report. Details on how to retrieve the report path are available at this [blog post](https://developers.exlibrisgroup.com/blog/Working-with-Analytics-REST-APIs).
+
---
+++ exl.js
@@ -0,0 +1,147 @@
+(function () {
+	var myConnector = tableau.makeConnector();
+	myConnector.init = function (initCallback) {
+		initCallback();
+		tableau.authType = tableau.authTypeEnum.custom;
+		var prefs = getPreferences();
+		if (tableau.phase == tableau.phaseEnum.authPhase) {
+			$("#reportUrl").css('display', 'none');
+			$('#txtApiKey').val(prefs.key);
+			if (prefs.key) {
+				tableau.password = prefs.key;
+				tableau.submit();
+			}
+		}
+	}
+
+	myConnector.getSchema = function (schemaCallback) {
+		var connectionData = JSON.parse(tableau.connectionData);
+		$.ajax({ 
+			url: connectionData.endpoint + 
+				'?path=' + connectionData.reportPath,
+			type: "GET",
+			beforeSend: function(xhr) { xhr.setRequestHeader('Authorization', 'apikey ' + tableau.password) }, 
+			success: function(data) {
+				var $xml = $( data );
+				var elements = $('xsd\\:element, element', $xml);
+				var cols = [];
+				elements.each(function(){
+					var $entry = $(this);
+					cols.push( { id: $entry.attr('name'), alias: $entry.attr('name'), dataType: mapDt($entry.attr('type'))});
+					// TODO: fact vs dimension?
+				})
+
+				var tableInfo = {
+					id: "exl_" + connectionData.reportName.toLowerCase().split(' ').join('_'),
+					alias: connectionData.reportName,
+					columns: cols
+				};
+
+				schemaCallback([tableInfo]);
+			}
+		});
+	};
+
+	myConnector.getData = function(table, doneCallback) {
+		getData(null, function(data) {
+			console.log('Data retrieved. Total rows', data.length);
+			table.appendRows(data);
+			doneCallback();
+		});
+	};
+
+	tableau.registerConnector(myConnector);
+
+	$(document).ready(function () {
+		var prefs = getPreferences();
+		$('#txtApiKey').val(prefs.key);
+		$("#submitButton").click(function () {
+			var reportPath = $('#txtReportPath').val();
+			if (reportPath.indexOf('/') >= 0) reportPath = encodeURIComponent(reportPath);
+			var connectionData = { 
+				endpoint: $('#selectEndpoint').val(),
+				reportPath: reportPath,
+				reportName: decodeURIComponent(reportPath).substring(decodeURIComponent(reportPath).lastIndexOf('/')+1),
+				maxRows: $('#selectMaxRows').val()
+			};
+			tableau.connectionData = JSON.stringify(connectionData);
+			tableau.password = $('#txtApiKey').val();
+			if ($('#chkRemember').prop('checked') ) {
+				prefs.key = $('#txtApiKey').val();
+			} else {
+				prefs.key = '';
+			}
+			setPreferences(prefs);
+			tableau.connectionName = "ExLibris";
+			tableau.submit();
+		});
+	});
+
+})();
+
+function mapDt(val) {
+	var types = { 
+		"xsd:int": tableau.dataTypeEnum.int,
+		"xsd:string": tableau.dataTypeEnum.string,
+		"xsd:double": tableau.dataTypeEnum.float,
+		"xsd:bool": tableau.dataTypeEnum.bool,
+		"xsd:date": tableau.dataTypeEnum.date
+	};
+
+	var type = types[val];
+	if (!type) type = tableau.dataTypeEnum.string;
+	return type;
+}
+
+var tableData;
+
+function getData(resumptionToken, callback) {
+	var connectionData = JSON.parse(tableau.connectionData);
+	var url = connectionData.endpoint;
+	if (resumptionToken) {
+		url = url + '?token=' + resumptionToken;
+	} else {
+		url = url + '?path=' + connectionData.reportPath;
+		tableData = [];
+	}
+	console.log('Calling url', url);
+	$.ajax({
+		url: url, 
+		type: "GET",
+		cache: false,
+		beforeSend: function(xhr) { xhr.setRequestHeader('Authorization', 'apikey ' + tableau.password) },
+		success: function(data) {
+			var $xml = $( data );
+			var token = resumptionToken || $('ResumptionToken', $xml).text();
+			var rows = $('Row', $xml);
+			rows.each(function(){
+				var $entry = $(this);
+				var obj = {};
+				$entry.children().each(function() {
+					obj[this.nodeName] = $(this).text();
+				});
+				tableData.push(obj);
+			});	
+			console.log('Added rows to data table. Total', tableData.length);
+			if ($('IsFinished', $xml).text() == 'false' && tableData.length < connectionData.maxRows) {
+				getData(token, callback);
+			} else {
+				callback(tableData);
+			}		
+		}
+	});
+}
+
+function getPreferences () {
+	var prefs;
+	try {
+		prefs = JSON.parse(atob(Cookies.get('prefs')))
+	} catch (e) {
+		prefs = { key: '' };
+	}
+	return prefs;
+}
+
+function setPreferences(prefs) {
+	Cookies.set('prefs', btoa(JSON.stringify(prefs)), { expires: 14 });
+}
